60e54fe4196488579a61fe49e6c440dfa82cc24e
Rakesh Menon <rmenon3@sapient.com>
2018-09-21 09:49:48 +0530

    Changed onClick to onChange

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Switch extends React.Component {
   render() {
-    const { on, onClick } = this.props;
+    const { on, onChange } = this.props;
     return (
-      <label className="switch" onClick={onClick}>
-        <input type="checkbox" checked={on} />
+      <label className="switch">
+        <input type="checkbox" checked={on} onChange={onChange} />
         <span className="slider round" />
       </label>
     );
